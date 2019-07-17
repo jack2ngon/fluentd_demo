7f0479c196a3dba394e14fb51ac12a6d66d0bba3
CuongHV <hoang.van.cuong@alliedtechbase.com>
2019-07-17 14:18:31 +0700

    .

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const io = require('socket.io')(http);
 const moment = require('moment');
 const _ = require('lodash');
 
-http.listen('1337', function(){
+http.listen('3004', function(){
     console.log('================ RUNNING SERVER ================');
-    console.log('Ready on port 1337.');
+    console.log('Ready on port 3004.');
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/efk_demo_db', {useNewUrlParser: true});
@@ -23,29 +23,14 @@ const ActivityLog = mongoose.models.ActivityLog || mongoose.model('ActivityLog',
 
 const getExistLogs = async () => {
     let logs = [];
-    logs = await ActivityLog.aggregate([
-        { "$group": {
-            "_id": {
-                "year": { "$year": "$created_at" },
-                "month": { "$month": "$created_at" },
-                "date": { "$dayOfMonth": "$created_at" },
-                "hour": { "$hour": "$created_at" },
-                "minute": { "$minute": "$created_at" },
-            },
-            total: { $sum: 1 }
-        }},
-
-        { $project : {
-            "_id": 0,
-            year: "$_id.year",
-            month: "$_id.month",
-            date: "$_id.date",
-            hour: "$_id.hour",
-            minute: "$_id.minute",
-            total : "$total" 
-        } },
-        {$sort:{"year":1, "month":1, "date":1, "hour":1, "minute":1}}
-    ]).allowDiskUse(true);
+    const loggingTime = moment();
+    for(let i = 1; i <= 300; i ++) {
+        const count = await ActivityLog.countDocuments({
+            "created_at" : { "$lt" : loggingTime.subtract(5 * (i - 1), 'seconds').toDate()},
+            "created_at" : { "$gte" : loggingTime.clone().subtract(5 * i, 'seconds').toDate()}
+        });
+        logs.push([loggingTime.subtract(5 * (i - 1), 'seconds').valueOf(), count]);
+    }
     return logs;
 };
 
@@ -61,23 +46,15 @@ const calculateTotalRequest = (logsData, currentLog) => {
 }
 
 io.sockets.on('connection', async (socket) => {
-  console.log('a user connected');
-    const existLogs = await getExistLogs();
-    const logsData = _.map(existLogs, (log) => {
-        log.time = moment().year(log.year).month(log.month - 1).date(log.date).hour(log.hour).minute(log.minute).second(0).valueOf(); 
-        return log;
-    });
-    const xLogs = _.sortBy(logsData, ['time']);
-    const xxLogs = [];
-    xLogs.forEach((log) => {
-        xxLogs.push([log.time, calculateTotalRequest(xLogs, log)]);
-    });
-    socket.emit('initial', xxLogs);
+    console.log('a user connected');
+    const logsData = await getExistLogs();
+    socket.emit('initial', logsData);
     setInterval(async () => {
         const executingTime = moment(); //.subtract(1, 'days');
         const count = await ActivityLog.countDocuments({
-            "created_at" : { "$lt" : executingTime.toDate()}
+            "created_at" : { "$lt" : executingTime.toDate()},
+            "created_at" : { "$gte" : executingTime.subtract(5, 'seconds').toDate()}
         });
         io.sockets.emit('refresh_chart', {time: executingTime.format('YYYY-MM-DD HH:MM:SS'),timestamp: executingTime.valueOf(), number_request: count});
-    }, 1000);
+    }, 5000);
 });
